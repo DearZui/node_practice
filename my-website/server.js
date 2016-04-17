0897acb0dc851951097918fe8588e750ddfb9d72
DearZui <673008724@163.com>
2016-04-18 00:20:35 +0800

    Ch08 Connect

---
+++ my-website/server.js
@@ -0,0 +1,33 @@
+var server = connect.createServer();
+
+server.use(connect.static(__dirname + '/website'));
+server.use(function (req, res, next) {
+    console.error(' %s %s ', req.method, req.url);
+    next();
+});
+
+server.user(function (req, res, next) {
+    if ('GET' == req.method && '/images' == req.url.substr(0, 7)) {
+        
+    } else {
+        next();
+    }
+    
+});
+
+server.use(function (req, res, next) {
+    if('GET' == req.method && '/' == req.url) {
+        
+    } else {
+        next();
+    }
+});
+
+server.use(function (req, res, next) {
+    res.writeHead(404);
+    res.end('Not Found');
+});
+
+app.use(connect.logger('dev'));
+
+server.listen(3000);
