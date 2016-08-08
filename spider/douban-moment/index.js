27741aafd827bb2bb5bfa1f4183938a28ec3aacb
dearzui <loebmraz@gmail.com>
2016-08-08 10:31:54 +0800

    spider-moment-somefunc

--- spider/douban-moment/index.js
+++ spider/douban-moment/index.js
@@ -26,8 +26,43 @@ function spiderContent(index) {
 			var $ = cheerio.load(html);
 			const article = {
 				title: $('#title', this).text(),
-				content: $('')
+				content: $('#id', this).html(),
+				picUrl: $('.content_img img', this).attr('src')
+			};
+			if (article) {
+				articles.push(movie);
 			}
-		})
+
+			downloadImg('../img/' + index + '/', article.picUrl);
+			saveData('./data' + index + '.json', articles);
+		});
+	}).on('error', function() {
+		console.log(err);
 	})
+}
+
+function downloadImg(imgDir, url) {
+    https.get(url, function (res) {
+        var data = '';
+        res.setEncoding('binary');
+        res.on('data', function (chunk) {
+            data += chunk;
+        });
+        res.on('end', function () {
+            fs.writeFile(imgDir + path.basename(url), data, 'binary', function (err) {
+                if (err) {
+                    return console.log(err);
+                }
+                console.log('Image downloaded: ', path.basename(url));
+            });
+        });
+    }).on('error', function (err) {
+        console.log(err);
+    });
+} ﻿​
+
+function* doSpider(x) {
+	var start = 141502;
+	console.log(start + '--------------------------');
+	
 }
