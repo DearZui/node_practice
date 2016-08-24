d667edb96aad781884caca4d2ea72642bac5141b
dearzui <673008724@163.com>
2016-08-24 16:48:56 +0800

    http-client

--- learnyounode/http-client.js
+++ learnyounode/http-client.js
@@ -3,7 +3,5 @@ const http = require('http');
 http.get(process.argv[2], function (res) {
 	res.setEncoding('utf-8');
 
-	res.on('data', function (data) {
-		console.log(data);
-	})
+	res.on('data', console.log);
 })
