8b76912ad9c879335bc85c252d7525e138ef150a
DearZui <673008724@163.com>
2016-11-04 16:09:44 +0800

    sciencenet

---
+++ spider/conference/sciencenet/index.js
@@ -0,0 +1,55 @@
+'use strict';
+
+const http = require('http'),
+	  fs = require('fs'),
+	  path = require('path'),
+	  cheerio = require('cheerio');
+
+const opt = {
+	hostname: 'meeting.sciencenet.cn/',
+	path: '/index.php',
+	port: 80
+};
+
+function spiderConf () {
+	http.get('http://meeting.sciencenet.cn/index.php?s=%2FCategory%2Fallmt&allid=1&p=1', function (res) {
+		var html = '';
+		var s_confs = [];
+		var date = new Date();
+		res.setEncoding('utf-8');
+
+		res.on('data', function (chunk) {
+			html += chunk;
+		});
+		res.on('end', ()=> {
+			var $ = cheerio.load(html);
+			var x = $('.content680').eq(1).children("table").first().children("tbody").children("tr");
+			x.map((e)=>{
+				if(e === 0) return;
+				var confUrl = {
+					m_url : x.eq(e).children("td").eq(1).children("a").attr("href"),
+					m_location: x.eq(e).children("td").eq(3).children("span").text().split("\t").pop().split(" ").shift(),
+					m_time: x.eq(e).children("td").eq(5).text().split(" ").join("").split("\n")[1],
+				};
+				if(confUrl) {
+					s_confs.push(confUrl);
+				}
+			});
+			saveData('./data' + date.getFullYear() + "-" + date.getMonth() + "-" + date.getDate() + '.json', s_confs);
+		});
+	}).on('error', (err) => {
+		console.log(err);
+	})
+}
+
+function saveData(path, confs) {
+	console.log(confs);
+	fs.writeFile(path, JSON.stringify(confs, null, ' '), (err)=>{
+		if (err) {
+			return console.log(err);
+		}
+		console.log('Data saved');
+	});
+}
+
+spiderConf();
