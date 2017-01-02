b38730e8a0ded612b73817594db31a2a3673262b
DearZui <673008724@163.com>
2017-01-02 10:23:10 +0800

    readme

---
+++ spider/README.md
@@ -0,0 +1,4 @@
+## Things need to be leanrt to write a web crawler.
+- ASYNC (1.2 - 1.5)
+- Ajax
+- request
--- spider/conference/sciencenet/index.js
+++ spider/conference/sciencenet/index.js
@@ -5,6 +5,11 @@ const http = require('http'),
 	  path = require('path'),
 	  cheerio = require('cheerio');
 
+//MongoDB
+const MongoClient = require('mongodb').MongoClient,
+			assert = require('assert');
+const url = 'mongodb://localhost:27017/sciencenet';
+
 const opt = {
 	hostname: 'meeting.sciencenet.cn/',
 	path: '/index.php',
@@ -36,7 +41,8 @@ function spiderConf (index) {
 					s_confs.push(confUrl);
 				}
 			});
-			saveData('./data/data' + date.getFullYear() + "-" + (date.getMonth()+1) + "-" + date.getDate() + "-" + index +'.json', s_confs);
+			console.log(s_confs);
+			//saveData('./data/data' + date.getFullYear() + "-" + (date.getMonth()+1) + "-" + date.getDate() + "-" + index +'.json', s_confs);
 		});
 	}).on('error', (err) => {
 		console.log(err);
@@ -64,4 +70,10 @@ function* doSpider(x) {
 
 for (var x of doSpider(10)) {
 	console.log(x);
+}
+
+
+var insertDocuments = function(db, documents, callback) {
+	var collection = db.collection('documents');
+	collection.insertMany
 }
