227890c4243da6f6ac60fcb6f5674746d18ff30a
DearZui <673008724@163.com>
2017-01-10 10:24:35 +0800

    escience update

--- spider/conference/escience/index.js
+++ spider/conference/escience/index.js
@@ -5,6 +5,29 @@ const http = require('http'),
 
 const opt = {
   hostname: 'csp.escience.cn',
-  path: '/dct/page/65537/__rpcsp0x2Main!65537%7C0_action/queryByStatus/__rpcsp0x2Main!65537%7C0_pageSize/50/__rpcsp0x2Main!65537%7C0_status/published/__rpcsp0x2Main!65537%7C0_pageNo/1?',
+  path: '/dct/page/65537/__rpcsp0x2Main!65537%7C0_action/queryByStatus/__rpcsp0x2Main!65537%7C0_pageSize/100/__rpcsp0x2Main!65537%7C0_status/published/__rpcsp0x2Main!65537%7C0_pageNo/1?',
   port: 80
 };
+
+function doSpider(index) {
+  http.get('http://csp.escience.cn/dct/page/65537/__rpcsp0x2Main!65537%7C0_action/queryByStatus/__rpcsp0x2Main!65537%7C0_pageSize/100/__rpcsp0x2Main!65537%7C0_status/published/__rpcsp0x2Main!65537%7C0_pageNo/' + index, function(res) {
+    var html = '';
+    var s_confs = [];
+		var date = new Date();
+		res.setEncoding('utf-8');
+
+		res.on('data', function (chunk) {
+			html += chunk;
+		});
+
+    res.on('end', () => {
+      var $ = cheerio.load(html);
+      var x = $("#confList>div[style$='10px;']");
+      x.map((e) => {
+        console.log(e);
+      })
+    })
+  })
+}
+
+doSpider(1);
